09c48cf5d92452ace088f81b95c9e89b2fba7c58
varunpats <varunpatil2502@gmail.com>
2023-01-23 11:33:58 +0530

    Initial commit

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
 //your code here
+const pNoSpan = document.getElementById('pNo');
+const list = document.getElementById('list');
+const next = document.getElementById('load_next');
+const prev = document.getElementById('load_prev');
+
+document.addEventListener("DOMContentLoaded", renderIssues);
+next.addEventListener("click", nextPage);
+prev.addEventListener("click", prevPage);
+
+let pageNo = 1;
+
+async function renderIssues() {
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+    pNoSpan.textContent = pageNo;
+    const url = `https://api.github.com/repositories/1296269/issues?page=${pageNo}&per_page=5`
+    const res = await fetch(url);
+    const data = await res.json();
+    console.log(data);
+    renderData(data);
+}
+
+function renderData(issues) {
+    issues.forEach(issue => {
+        const li = document.createElement('li');
+        li.textContent = issue.title;
+        list.appendChild(li);
+    });
+}
+
+function nextPage() {
+    pageNo += 1;
+    renderIssues()
+}
+
+function prevPage() {
+    if(pageNo == 1)
+        return
+    pageNo -= 1;
+    renderIssues()
+}
